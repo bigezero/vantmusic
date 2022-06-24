50be3499522f66a9cb92e7db51cdebe3c6d52dd3
bigezero <2415774857@qq.com>
2022-06-24 14:07:16 +0800

    ready

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+//引入字体适配
+import "@/mobile/flexible"
+//引入清除默认样式
+import "@/styles/reset.css"
+import router from "@/router"
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>我是Layout</p>
+    <!-- 二级路由 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .main{
+    padding-top: 46px;
+    padding-bottom: 50px;
+  }
+</style>
