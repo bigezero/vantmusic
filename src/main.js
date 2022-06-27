a9697ebfa1f275edde0cebc50184427e3369c329
bigezero <2415774867@qq.com>
2022-06-27 18:27:56 +0800

    add vant code

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import "@/mobile/flexible"
 import "@/styles/reset.css"
 import router from "@/router"
 
+import { Tabbar, TabbarItem } from 'vant';
+
+Vue.use(Tabbar);
+Vue.use(TabbarItem);
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,18 +3,28 @@
     <p>我是Layout</p>
     <!-- 二级路由 -->
     <router-view></router-view>
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="search">标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      active: 0,
+    };
+  },
+};
 </script>
 
 <style scoped>
-  .main{
-    padding-top: 46px;
-    padding-bottom: 50px;
-  }
+.main {
+  padding-top: 46px;
+  padding-bottom: 50px;
+}
 </style>
